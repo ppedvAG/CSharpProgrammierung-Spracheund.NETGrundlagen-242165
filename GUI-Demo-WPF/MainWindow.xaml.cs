deb81e85bcf4ec27a6818d9580ccb4e9b00ee095
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-12-20 12:10:44 +0100

    Module 014 WPF Demo Applikation

---
+++ GUI-Demo-WPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI_Demo_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoSth_Click(object sender, RoutedEventArgs e)
+        {
+            var name = txtInput.Text;
+            var anrede = GetAnrede(cbxOptions);
+
+            MessageBox.Show($"Hallo {anrede} {name}");
+        }
+
+        private string GetAnrede(ComboBox cbxOptions)
+        {
+            Enum.TryParse(cbxOptions.SelectedValue.ToString(), out Sex selected);
+            switch (selected)
+            {
+                case Sex.Male:
+                    return "Herr";
+                case Sex.Female:
+                    return "Frau";
+                default:
+                    return "";
+            }
+        }
+    }
+
+    enum Sex
+    {
+        Male,
+        Female,
+        Divers
+    }
+}
